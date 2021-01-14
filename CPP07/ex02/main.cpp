8469be020525af207487103083ebd147b0a2379c
stella <estelle.slrd@gmail.com>
2021-01-14 13:14:32 +0100

    added cpp07 ex02 better main

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: esoulard <esoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 12:34:17 by esoulard          #+#    #+#             */
-/*   Updated: 2021/01/13 17:18:35 by esoulard         ###   ########.fr       */
+/*   Updated: 2021/01/14 12:57:52 by esoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,58 +17,72 @@ int main(void) {
 	std::cout << std::endl << "-------------------------------------" << std::endl;
 	std::cout << "--------PRINT STRING ARRAY[3]--------" << std::endl << std::endl;
 
-	Array< std::string > blou(3);
+	Array< std::string > strArray(3);
 
-	std::cout << "[EMPTY]" << std::endl;
-	for (int i = 0; i < blou.size(); i++)
-		std::cout << blou[i] << std::endl;
+	std::cout << "[INIT BY DEFAULT]" << std::endl;
+	for (int i = 0; i < strArray.size(); i++)
+		std::cout << strArray[i] << std::endl;
 
-	blou[0] = "omg im a string";
-	blou[1] = "omg me too";
-	blou[2] = "im not a string, im a panda";
+	strArray[0] = "omg im a string";
+	strArray[1] = "omg me too";
+	strArray[2] = "im not a string, im a panda";
 
-	std::cout << "[FULL]" << std::endl;
-	for (int i = 0; i < blou.size(); i++)
-		std::cout << blou[i] << std::endl;
+	std::cout << "[AFTER ASSIGNING]" << std::endl;
+	for (int i = 0; i < strArray.size(); i++)
+		std::cout << strArray[i] << std::endl;
 
 	std::cout << std::endl << "-----------------------------------" << std::endl;
 	std::cout << "--------DEFAULT CONSTRUCTOR--------" << std::endl << std::endl;
 
-	Array< std::string > bla;
+	Array< std::string > emptyStrArray;
 
-	std::cout << "[EMPTY]" << std::endl;
-	for (int i = 0; i < bla.size(); i++)
-		std::cout << bla[i] << std::endl;
+	std::cout << "[INIT BY DEFAULT]" << std::endl;
+	for (int i = 0; i < emptyStrArray.size(); i++)
+		std::cout << emptyStrArray[i] << std::endl;
 
 	std::cout << std::endl << "----------------------------------------" << std::endl;
 	std::cout << "--------OPERATOR= ON EMPTY ARRAY--------" << std::endl << std::endl;
-	bla = blou;
+	emptyStrArray = strArray;
 
-	for (int i = 0; i < bla.size(); i++)
-		std::cout << bla[i] << std::endl;
+	for (int i = 0; i < emptyStrArray.size(); i++)
+		std::cout << emptyStrArray[i] << std::endl;
 
 	std::cout << std::endl << "---------------------------------------" << std::endl;
 	std::cout << "--------OPERATOR= ON FULL ARRAY--------" << std::endl << std::endl;
-	Array< std::string > blep(1);
+	Array< std::string > anotherStrArray(1);
 
-	blep[0] = "A BOOMBOX IS NOT A TOY!!!";
+	anotherStrArray[0] = "A BOOMBOX IS NOT A TOY!!!";
 
-	blou = blep;
-	for (int i = 0; i < blou.size(); i++)
-		std::cout << blou[i] << std::endl;
+	strArray = anotherStrArray;
+	for (int i = 0; i < anotherStrArray.size(); i++)
+		std::cout << anotherStrArray[i] << std::endl;
+
+	for (int i = 0; i < strArray.size(); i++)
+		std::cout << strArray[i] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[MODIFYING ONE DOESNT AFFECT THE OTHER]" << std::endl << std::endl;
+
+	strArray[0] = "AFRIDIBIDIHUUUU!";
+
+	for (int i = 0; i < anotherStrArray.size(); i++)
+		std::cout << anotherStrArray[i] << std::endl;
+	for (int i = 0; i < strArray.size(); i++)
+		std::cout << strArray[i] << std::endl;
 
 	std::cout << std::endl << "--------------------------------" << std::endl;
 	std::cout << "--------COPY CONSTRUCTOR--------" << std::endl << std::endl;
-	Array< std::string > flap(blep);
+	Array< std::string > copiedStrArray(anotherStrArray);
 
-	for (int i = 0; i < flap.size(); i++)
-		std::cout << flap[i] << std::endl;
+	for (int i = 0; i < copiedStrArray.size(); i++)
+		std::cout << copiedStrArray[i] << std::endl;
 
 	std::cout << std::endl << "----------------------------------" << std::endl;
 	std::cout << "--------PRINT INT ARRAY[5]--------" << std::endl << std::endl;
 	std::cout << std::endl;
 	Array< int > 	a(5);
 
+	std::cout << "[INIT BY DEFAULT]" << std::endl;
 	for (int i = 0; i < a.size(); i++)
 		std::cout << a[i] << std::endl;
 	
@@ -84,28 +98,35 @@ int main(void) {
 	std::cout << std::endl << "------------------------------------" << std::endl;
 	std::cout << "--------PRINT FLOAT ARRAY[2]--------" << std::endl << std::endl;
 	std::cout << std::endl;
-	Array< float > 	sneak(2);
+	Array< float > 	sneakyFloat(2);
 
-	for (int i = 0; i < sneak.size(); i++)
-		std::cout << sneak[i] << std::endl;
+	std::cout << "[INIT BY DEFAULT]" << std::endl;
+	for (int i = 0; i < sneakyFloat.size(); i++)
+		std::cout << sneakyFloat[i] << std::endl;
 	
 	std::cout << std::endl << "-----------------------------------------------" << std::endl;
 	std::cout << "--------ITERATE & MODIFY FLOAT ARRAY[2]--------" << std::endl << std::endl;
 	std::cout << std::endl;
 
-	for (int i = 0; i < sneak.size(); i++) {
-		sneak[i] += i;
-		std::cout << sneak[i] << std::endl;
+	for (int i = 0; i < sneakyFloat.size(); i++) {
+		sneakyFloat[i] += i;
+		std::cout << sneakyFloat[i] << std::endl;
 	}
 
 	std::cout << std::endl << "-----------------------------------" << std::endl;
 	std::cout << "--------BAD INDEX EXCEPTION--------" << std::endl << std::endl;
 	try {
-		std::cout << "Array a size " << a.size() << ", getting index 42" << std::endl;
+		std::cout << "Array a size <" << a.size() << ">, getting index <2>..." << std::endl;
+		std::cout << a[2] << std::endl;
+		std::cout << "Index 2 successfully retrieved!" << std::endl;
+		
+		std::cout << std::endl;
+		std::cout << "Array a size <" << a.size() << ">, getting index <42>..." << std::endl;
 		std::cout << a[42] << std::endl;
+		std::cout << "Index 42 successfully retrieved!" << std::endl;
 	}
 	catch(std::exception &e) {
-		std::cerr << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
 
 	return 0;
