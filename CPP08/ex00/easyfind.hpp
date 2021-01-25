6dcd1b98be7d7009e25afd9cedac71f46fec78d4
stella <estelle.slrd@gmail.com>
2021-01-25 11:17:58 +0100

    cpp08 ex00 fix

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,28 +6,24 @@
 /*   By: esoulard <esoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 17:15:24 by esoulard          #+#    #+#             */
-/*   Updated: 2021/01/25 10:47:47 by esoulard         ###   ########.fr       */
+/*   Updated: 2021/01/25 11:16:58 by esoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <memory>
 #include "customException.hpp"
 
 template <typename Container>
 typename Container::const_iterator easyfind(Container const &var1, int const var2) {
 
 	typename Container::const_iterator it;
-	typename Container::const_iterator ite = var1.end();
-	
-	for (it = var1.begin(); it != ite; it++) {
-	 	if (*it == var2)
-	 		return it;
-	}
-	throw customException("Couldn't find value");
+
+	it = std::find(var1.begin(), var1.end(), var2);
+	if (it == var1.end())
+		throw customException("Couldn't find value");
+	return (it);
 }
 
 #endif
-
