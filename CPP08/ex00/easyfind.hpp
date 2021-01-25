2c814c094b5ab9cb349a8ae1380972e1046f9820
stella <estelle.slrd@gmail.com>
2021-01-25 10:48:39 +0100

    cpp08 ex00 betterized

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: esoulard <esoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 17:15:24 by esoulard          #+#    #+#             */
-/*   Updated: 2021/01/24 16:51:03 by esoulard         ###   ########.fr       */
+/*   Updated: 2021/01/25 10:47:47 by esoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,11 @@
 #include <memory>
 #include "customException.hpp"
 
-//template <containerTemplate<int, nextT> classT>
+template <typename Container>
+typename Container::const_iterator easyfind(Container const &var1, int const var2) {
 
-/* 
-** easyFind() prototype:
-** takes a T<int, nextT>
-** returns an iterator on T<int, next> and an int
-*/
-
-template <template <typename, typename> class T>
-typename T<int, std::allocator<int>>::const_iterator easyfind(T<int, std::allocator<int>> const &var1, int const var2) {
-
-	typename T<int, std::allocator<int>>::const_iterator it;
-	typename T<int, std::allocator<int>>::const_iterator ite = var1.end();
+	typename Container::const_iterator it;
+	typename Container::const_iterator ite = var1.end();
 	
 	for (it = var1.begin(); it != ite; it++) {
 	 	if (*it == var2)
